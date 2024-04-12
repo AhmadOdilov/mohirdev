db9a38f92363e5c91f6aa1f6752e4ea4047bcc0a
AkhmadbekOdilov <134781457+AhmadOdilov@users.noreply.github.com>
2024-04-12 16:28:42 +0500

    6-modul 1-dars

---
+++ 6-modul/accouts/forms.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = ('username', 'first_name', 'last_name','age', 'email', )
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'first_name','last_name', 'age', 'email', )
